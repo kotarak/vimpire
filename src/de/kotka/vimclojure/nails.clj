b8b9a7852e8755dc19c246990ca181c3f5605fb7
Meikel Brandmeyer <mb@kotka.de>
2009-04-04 14:09:12 +0200

    Require backend and util instead of using them
    --HG--
    branch : bleeding-edge

--- src/de/kotka/vimclojure/nails.clj
+++ src/de/kotka/vimclojure/nails.clj
@@ -22,19 +22,11 @@
 
 (clojure.core/ns de.kotka.vimclojure.nails
   (:use
-     (de.kotka.vimclojure [util :only (with-command-line
-                                       clj->vim
-                                       safe-var-get
-                                       decide-completion-in
-                                       pretty-print
-                                       pretty-print-code
-                                       make-completion-item
-                                       resolve-and-load-namespace
-                                       stream->seq)]
-                          backend)
      [clojure.contrib.def :only (defvar)])
   (:require
-     [de.kotka.vimclojure.repl :as repl])
+     (de.kotka.vimclojure [repl :as repl]
+                          [util :as util]
+                          [backend :as backend]))
   (:import
      com.martiansoftware.nailgun.NGContext
      clojure.lang.LineNumberingPushbackReader
@@ -65,7 +57,7 @@
                            LineNumberingPushbackReader.)
                  ~'*out* (-> ~'nailContext .out OutputStreamWriter.)
                  ~'*err* (-> ~'nailContext .err PrintWriter.)]
-         (with-command-line (.getArgs ~'nailContext)
+         (util/with-command-line (.getArgs ~'nailContext)
            ~usage
            ~arguments
            ~@body)
@@ -75,9 +67,9 @@
 (defnail DocLookup
   "Usage: ng de.kotka.vimclojure.nails.DocString [options]"
   [[nspace n "Lookup the symbols in the given namespace." "user"]]
-  (let [nspace  (resolve-and-load-namespace nspace)
+  (let [nspace  (util/resolve-and-load-namespace nspace)
         symbols (map symbol (line-seq (BufferedReader. *in*)))]
-    (print (doc-lookup nspace symbols))
+    (print (backend/doc-lookup nspace symbols))
     (flush)))
 
 (defnail FindDoc
@@ -90,27 +82,30 @@
 (defnail JavadocPath
   "Usage: ng de.kotka.vimclojure.nails.JavadocPath [options]"
   [[nspace n "Lookup the symbols in the given namespace." "user"]]
-  (let [nspace         (resolve-and-load-namespace nspace)
+  (let [nspace         (util/resolve-and-load-namespace nspace)
         our-ns-resolve #(ns-resolve nspace %)]
-    (doseq [path (map #(-> % symbol our-ns-resolve javadoc-path-for-class)
-                      (stream->seq *in*))]
+    (doseq [path (map #(-> %
+                         symbol
+                         our-ns-resolve
+                         backend/javadoc-path-for-class)
+                      (util/stream->seq *in*))]
       (println path))))
 
 (defnail MetaLookup
   "Usage: ng de.kotka.vimclojure.nails.MetaLookup [options]"
   [[nspace n "Lookup the symbols in the given namespace." "user"]]
-  (let [nspace         (resolve-and-load-namespace nspace)
+  (let [nspace         (util/resolve-and-load-namespace nspace)
         our-ns-resolve #(ns-resolve nspace %)]
     (doseq [metainfo (map #(-> % symbol our-ns-resolve meta)
-                     (stream->seq *in*))]
-      (pretty-print metainfo))))
+                     (util/stream->seq *in*))]
+      (util/pretty-print metainfo))))
 
 (defnail DynamicHighlighting
   "Usage: ng de.kotka.vimclojure.nails.DynamicHighlighting"
   []
   (let [nspace    (read)
         c-c       (the-ns 'clojure.core)
-        the-space (resolve-and-load-namespace nspace)
+        the-space (util/resolve-and-load-namespace nspace)
         refers    (remove #(= c-c (-> % second meta :ns)) (ns-refers the-space))
         aliases   (mapcat (fn [[the-alias the-alias-space]]
                             (map #(vector (symbol (name the-alias)
@@ -128,7 +123,7 @@
         vars      (set (concat refers aliases namespaces))
         macros    (set (filter #(-> % second meta :macro) vars))
         vars      (clojure.set/difference vars macros)
-        fns       (set (filter #(let [v (safe-var-get (second %))]
+        fns       (set (filter #(let [v (util/safe-var-get (second %))]
                                   (or (fn? v)
                                       (instance? clojure.lang.MultiFn v)))
                                vars))
@@ -136,14 +131,14 @@
     (-> (hash-map "Func"     (map first fns)
                   "Macro"    (map first macros)
                   "Variable" (map first vars))
-      clj->vim
+      util/clj->vim
       println)))
 
 (defnail NamespaceOfFile
   "Usage: ng de.kotka.vimclojure.nails.NamespaceOfFile"
   []
   (let [of-interest '#{in-ns ns clojure.core/in-ns clojure.core/ns}
-        in-seq      (stream->seq *in*)
+        in-seq      (util/stream->seq *in*)
         candidate   (first
                       (drop-while #(or (not (instance? clojure.lang.ISeq %))
                                        (not (contains? of-interest (first %))))
@@ -159,20 +154,20 @@
 (defnail NamespaceInfo
   "Usage: ng de.kotka.vimclojure.nails.NamespaceInfo"
   []
-  (println (clj->vim (map #(-> % symbol find-ns ns-info)
-                          (line-seq (BufferedReader. *in*))))))
+  (println (util/clj->vim (map #(-> % symbol find-ns backend/ns-info)
+                               (line-seq (BufferedReader. *in*))))))
 
 (defnail MacroExpand
   "Usage: ng de.kotka.vimclojure.nails.MacroExpand [options]"
   [[nspace n "Lookup the symbols in the given namespace." "user"]
    [one?   o "Expand only the first macro."]]
-  (let [nspace (resolve-and-load-namespace nspace)
+  (let [nspace (util/resolve-and-load-namespace nspace)
         expand (if one
                  #(macroexpand-1 %)
                  #(macroexpand %))]
     (binding [*ns* nspace]
-      (doseq [expr (stream->seq *in*)]
-        (-> expr expand pretty-print-code)))))
+      (doseq [expr (util/stream->seq *in*)]
+        (-> expr expand util/pretty-print-code)))))
 
 (defnail Repl
   "Usage: ng de.kotka.vimclojure.nails.Repl [options]"
@@ -186,7 +181,7 @@
   (let [id     (Integer/parseInt id)
         line   (Integer/parseInt line)
         nspace (when (not= nspace "")
-                 (resolve-and-load-namespace nspace))]
+                 (util/resolve-and-load-namespace nspace))]
     (cond
       start (println (repl/start))
       stop  (repl/stop id)
@@ -196,7 +191,7 @@
   "Usage: ng de.kotka.vimclojure.nails.CheckSyntax"
   []
   (try
-    (dorun (stream->seq *in*))
+    (dorun (util/stream->seq *in*))
     (println true)
     (catch Exception e
       (println false))))
@@ -206,11 +201,12 @@
   [[nspace n "Start completion in this namespace." "user"]
    [prefix p "Prefix used for the match, ie. the part before /." ""]
    [base   b "Base pattern to be matched."]]
-  (let [nspace (resolve-and-load-namespace nspace)
+  (let [nspace (util/resolve-and-load-namespace nspace)
         prefix (symbol prefix)]
     (if-not (and (= base "") (= prefix ""))
-      (let [to-complete (decide-completion-in nspace prefix base)
-            completions (mapcat #(complete % nspace prefix base) to-complete)
-            completions (map #(apply make-completion-item %) completions)]
-        (println (clj->vim completions)))
+      (let [to-complete (util/decide-completion-in nspace prefix base)
+            completions (mapcat #(backend/complete % nspace prefix base)
+                                to-complete)
+            completions (map #(apply util/make-completion-item %) completions)]
+        (println (util/clj->vim completions)))
       (println "[]"))))

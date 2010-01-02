4fbe8bf28954a622bcede06cfcd1e638d3aeac50
Meikel Brandmeyer <mb@kotka.de>
2010-01-02 22:54:10 +0100

    Remove installers for now
    Neither Justin nor me can support them at the moment.

--- installers/windows/build.bat
+++
@@ -1,9 +0,0 @@
-pushd ..\..
-rmdir /s/q tmpvimfiles
-echo vimdir=tmpvimfiles> local.properties
-cmd /c ant
-cmd /c ant install
-copy ng.exe installers\windows\bin\ng.exe
-cmd /c makensis installers\windows\VimClojure.nsi
-rmdir /s/q tmpvimfiles
-popd

6ffa1e5ce41beaced9db3f94005957a901255dc8
Meikel Brandmeyer <mb@kotka.de>
2018-03-01 18:31:47 +0100

    Add actions map file for unrepl blov generation

---
+++ actions.clj
@@ -0,0 +1,35 @@
+{:vimpire.nails/doc-lookup
+ (vimpire.nails/doc-lookup #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/find-doc
+ (vimpire.nails/find-doc #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/javadoc-path
+ (vimpire.nails/javadoc-path #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/source-lookup
+ (vimpire.nails/source-lookup #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/source-location
+ (vimpire.nails/source-location #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/meta-lookup
+ (vimpire.nails/meta-lookup #unrepl/param :nspace #unrepl/param :sym)
+
+ :vimpire.nails/dynamic-highlighting
+ (vimpire.nails/dynamic-highlighting #unrepl/param :nspace)
+
+ :vimpire.nails/namespace-of-file
+ (vimpire.nails/namespace-of-file #unrepl/param :content)
+
+ :vimpire.nails/namespace-info
+ (vimpire.nails/namespace-info #unrepl/param :content)
+
+ :vimpire.nails/macro-expand
+ (vimpire.nails/macro-expand #unrepl/param :nspace #unrepl/param :form #unrepl/param :one?)
+
+ :vimpire.nails/complete
+ (vimpire.nails/complete #unrepl/param :nspace #unrepl/param :prefix #unrepl/param :base)
+
+ :vimpire.nails/run-tests
+ (vimpire.nails/run-tests #unrepl/param :nspace #unrepl/param :all?)}
